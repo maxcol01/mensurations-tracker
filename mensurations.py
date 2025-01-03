c155b8921cc388adfca94123503ed0704eef1f8d
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2025-01-03 16:37:50 +0100

    added the main file of the app

---
+++ mensurations.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Setting of the app
+st.set_page_config(page_title="Mensurations App")
+st.title("Mensurations Tracker App")
+
+# Mensurations to track
+mensurations = ["Torse", "cou", "taille", "hanches", "cuisses", "ventre", "poids"]
